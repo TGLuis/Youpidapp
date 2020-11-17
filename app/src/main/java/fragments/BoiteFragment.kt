54cf1cb15660912fc67b626f3ec12568ace3307e
FrancoisDuchene <duchene.francois@protonmail.ch>
2020-11-17 23:40:40 +0100

    DES VRAIS BUZZERS !!!!!!!!

--- app/src/main/java/fragments/BoiteFragment.kt
+++ app/src/main/java/fragments/BoiteFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.appcompat.widget.AppCompatImageButton
 import lufra.youpidapp.MainActivity
 import lufra.youpidapp.R
 
